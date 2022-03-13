57c92c4c1b3a844dd5ee631b0947370afb554a5c
2214yj <2214yjgm@gmail.com>
2022-03-13 19:07:11 +0900

    REFACTOR: Refactor User Entity
    ** User Entity의 id 값을 bigint 형으로 변경, primary key가 자동 증가하도록 변경

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,23 +1,24 @@
 import "reflect-metadata";
-import {Column, Entity, PrimaryGeneratedColumn, Timestamp} from "typeorm";
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Timestamp} from "typeorm";
 
 @Entity("User", { schema: "hangaround-1" })
 export class User {
-  @PrimaryGeneratedColumn({ type: "int", name: "user_id" })
+  @PrimaryGeneratedColumn('increment',{ type: "bigint", name: "user_id" })
   userId: number;
 
-  @Column("int",{name: "sns_id"})
+  @Column("bigint",{name: "sns_id"})
   snsId: number;
 
-  @Column("varchar", { name: "user_name", length: 255 })
+  @Column("varchar", { name: "user_name", length: 15 })
   userName: string;
 
-  @Column("varchar", { name: "channel", length: 255 })
+  @Column("varchar", { name: "channel", length: 15 })
   channel: string;
 
   @Column("int", { name: "room_id" })
   roomId: number;
 
-  @Column("datetime",{name: "created_at"})
-  createdAt: Timestamp;
+  @CreateDateColumn({ name: 'created_at' ,type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)"})
+  createdAt: Date;
+
 }
