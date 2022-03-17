3b7b0fe77f29fb22c6acdede4f545af574f3758f
Jinjoo Rho <estelle3785@gmail.com>
2022-03-17 16:40:51 +0900

    REFACTOR: Refactor new_room.js add room_code, add owner_id

--- src/routes/new_room.js
+++ src/routes/new_room.js
@@ -10,7 +10,6 @@ router.post('/', async (req,res,next)=> {
       .insert()
       .into(Room)
       .values({ 
-        roomId: req.body.roomId, 
         roomName: req.body.roomName,
         maxPlayer: req.body.maxPlayer,
         gameId: 0 //일단 new_room에서는 무조건 default_game으로 주기
