330f7f8be5ef252b8811e15e9ae0a3fd1e252f8e
Jinjoo Rho <estelle3785@gmail.com>
2022-03-17 18:03:45 +0900

    REFACTOR: Send room_code in JSON format, refactor error code

--- src/routes/new_room.js
+++ src/routes/new_room.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getRepository, getConnection } = require("typeorm");
+const {getConnection} = require("typeorm");
 const {Room} = require("../entities/Room");
+const baseResponseStatus = require("../../config/baseResponseStatus");
+
+var room_code = Math.random().toString(36).slice(2);
 
 router.post('/', async (req,res,next)=> {
   try {
@@ -12,16 +15,22 @@ router.post('/', async (req,res,next)=> {
       .values({ 
         roomName: req.body.roomName,
         maxPlayer: req.body.maxPlayer,
-        gameId: 0 //일단 new_room에서는 무조건 default_game으로 주기
+        roomCode: room_code,
+        ownerId: req.body.ownerId,
+        playerCnt: 0, //나중에 이거랑 밑에거 DB에서 바꾸기
+        gameId: 1 //일단 new_room에서는 무조건 default_game으로 주기
       })
       .execute()
-      res.status(200).json({
-        success: true
-      });
+      res.send(baseResponseStatus.SUCCESS);
     } catch (error) {
         console.error(error);
-        next(error);
+        next(baseResponseStatus.DB_INSERT_ERROR);
       }
+});
+
+router.use(function (req, res, next){
+  var json_room_code = { 'roomCode': room_code };
+  res.send(json_room_code);
 });
 
 module.exports = router;
