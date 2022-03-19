ea51b12ffe5e5460bcfbfff977b48a22ef6086b2
2214yj <2214yjgm@gmail.com>
2022-03-20 06:02:39 +0900

    FEAT: Add withoutLogin feature
    ** withoutLogin 구현

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const passport = require('passport');
 const {User} = require("../entities/User");
 const {getRepository} = require("typeorm");
+const baseResponse = require("../../config/baseResponseStatus");
+const {response} = require("../../config/response");
 
 const router = express.Router();
 
@@ -12,5 +14,19 @@ router.get('/kakao/callback', passport.authenticate('kakao', {
 }), (req, res) => {
     res.redirect('/main');
 });
+
+router.get('/without-login', async (req, res, next) => {
+    try {
+        const userRepository = getRepository(User);
+        const newUser = await userRepository.create({
+            channel: "local"
+        });
+        await userRepository.save(newUser);
+        res.send(response(baseResponse.SUCCESS));
+    } catch (error) {
+        console.error(error);
+        next(baseResponse.DB_ERROR)
+    }
+})
 
 module.exports = router;
