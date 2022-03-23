1a8407b1bfcb96edc04f376d4c0fa45502dd843a
Jinjoo Rho <estelle3785@gmail.com>
2022-03-23 20:14:58 +0900

    REFACTOR: Refactor routes structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.use(express.json());
 
 const auth = require('./auth.js');
-const new_room = require('./new_room.js');
-const join_room = require('./join_room.js')
+const roomRoutes = require('./room/index.js');
+//const room = require(roomRouter);
+//const new_room = require('./room/new_room.js');
+//const join_room = require('./join_room.js')
 
 router.use('/auth', auth);
-router.use('/new_room', new_room);
-router.use('/join_room', join_room);
+router.use('/room', roomRoutes);
+//router.use('/new_room', new_room);
+//router.use('/join_room', join_room);
 
 
 router.get('/main', async (req, res) => {
---
+++ src/routes/room/room.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+console.log("room 구현중");
+
+module.exports = router;
