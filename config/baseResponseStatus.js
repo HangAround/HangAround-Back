0749ca2a027204613101c118bfdbb3461fe8d747
2214yj <2214yjgm@gmail.com>
2022-03-24 20:55:05 +0900

    FIX: Fix URL Path

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -18,15 +18,15 @@ module.exports = {
     ROOM_CAPACITY_EXCESS_ERROR: {"isSuccess": false, "code": 2005, "message": "[정원 초과] 입장할 수 없는 방입니다."},
     ROOM_STATUS_ERROR: {"isSuccess": false, "code": 2006, "message": "[게임 진행중] 입장할 수 없는 방입니다."},
     ROOM_CAPACITY_ERROR: {"isSuccess": false, "code": 2007, "message": "방 정원(4~6)을 확인해주세요."},
-
+    ROOM_ROOMID_NOT_EXIST: {"isSuccess": false, "code": 3010, "message": "해당 방이 존재하지 않습니다."},
+    USER_ROOMID_NOT_EXIST: {"isSuccess": false, "code": 3011, "message": "해당 방에 입장한 유저가 존재하지 않습니다."},
 
     // Response error
     SIGNIN_WITHDRAWAL_ACCOUNT: {"isSuccess": false, "code": 3001, "message": "탈퇴 된 계정입니다. 고객센터에 문의해주세요."},
     NEW_ROOM_ERROR: {"isSuccess": false, "code": 3002, "message": "방 생성에 실패했습니다."},
     JOIN_ROOM_ERROR: {"isSuccess": false, "code": 3003, "message": "방 입장에 실패했습니다."},
     ROOMINFO_MODIFY_ERROR: {"isSuccess": false, "code": 3004, "message": "방 정보 수정에 실패했습니다."},
-    ROOM_ROOMID_NOT_EXIST: {"isSuccess": false, "code": 3010, "message": "해당 방이 존재하지 않습니다."},
-    USER_ROOMID_NOT_EXIST: {"isSuccess": false, "code": 3011, "message": "해당 방에 입장한 유저가 존재하지 않습니다."},
+
 
     //Connection, Transaction 등의 서버 오류
     DB_ERROR: {"isSuccess": false, "code": 4000, "message": "데이터 베이스 에러"},
