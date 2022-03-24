2bb7fc4fce740fb1ff0a914bfdf871184c01253a
Jinjoo Rho <estelle3785@gmail.com>
2022-03-24 17:42:21 +0900

    FEATURE: Add Room code dupulication check feature #17

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.use(express.json());
 
 const auth = require('./auth.js');
 const roomRoutes = require('./room/index.js');
-//const room = require(roomRouter);
-//const new_room = require('./room/new_room.js');
-//const join_room = require('./join_room.js')
 
 router.use('/auth', auth);
 router.use('/room', roomRoutes);
-//router.use('/new_room', new_room);
-//router.use('/join_room', join_room);
-
 
 router.get('/main', async (req, res) => {
     res.send("메인 화면");
