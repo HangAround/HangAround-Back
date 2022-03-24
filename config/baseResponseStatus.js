ec63f34906f73062a05400a33377874f35128b84
sumin <3189min@naver.com>
2022-03-24 19:17:21 +0900

    FEAT: Feat roomInfo-modify #22

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -3,29 +3,30 @@
 module.exports = {
 
     // Success
-    SUCCESS : { "isSuccess": true, "code": 1000, "message":"성공" },
+    SUCCESS: {"isSuccess": true, "code": 1000, "message": "성공"},
 
     // Common
-    TOKEN_EMPTY : { "isSuccess": false, "code": 2000, "message":"JWT 토큰을 입력해주세요." },
-    TOKEN_VERIFICATION_FAILURE : { "isSuccess": false, "code": 3000, "message":"JWT 토큰 검증 실패" },
-    TOKEN_VERIFICATION_SUCCESS : { "isSuccess": true, "code": 1001, "message":"JWT 토큰 검증 성공" },
+    TOKEN_EMPTY: {"isSuccess": false, "code": 2000, "message": "JWT 토큰을 입력해주세요."},
+    TOKEN_VERIFICATION_FAILURE: {"isSuccess": false, "code": 3000, "message": "JWT 토큰 검증 실패"},
+    TOKEN_VERIFICATION_SUCCESS: {"isSuccess": true, "code": 1001, "message": "JWT 토큰 검증 성공"},
 
     // Request error
-    SIGNIN_EMAIL_EMPTY : { "isSuccess": false, "code": 2001, "message":"이메일을 입력해주세요" },
-    ROOM_NAME_EMPTY : { "isSuccess": false, "code": 2002, "message": "방 이름을 입력해주세요." },
-    USER_NAME_EMPTY : { "isSuccess": false, "code": 2003, "message":"닉네임을 입력해주세요" },
-    ROOM_CODE_ERROR : { "isSuccess": false, "code": 2004, "message":"유효하지 않은 코드 번호입니다" },
-    ROOM_CAPACITY_EXCESS_ERROR : { "isSuccess": false, "code": 2005, "message":"[정원 초과] 입장할 수 없는 방입니다." },
-    ROOM_STATUS_ERROR : { "isSuccess": false, "code": 2006, "message":"[게임 진행중] 입장할 수 없는 방입니다." },
-    
-    // Response error
-    SIGNIN_WITHDRAWAL_ACCOUNT : { "isSuccess": false, "code": 3001, "message": "탈퇴 된 계정입니다. 고객센터에 문의해주세요." },
-    NEW_ROOM_ERROR : { "isSuccess": false, "code": 3002, "message": "방 생성에 실패했습니다." },
-    JOIN_ROOM_ERROR : { "isSuccess": false, "code": 3003, "message": "방 입장에 실패했습니다." }, 
+    SIGNIN_EMAIL_EMPTY: {"isSuccess": false, "code": 2001, "message": "이메일을 입력해주세요."},
+    ROOM_NAME_EMPTY: {"isSuccess": false, "code": 2002, "message": "방 이름을 입력해주세요."},
+    USER_NAME_EMPTY: {"isSuccess": false, "code": 2003, "message": "닉네임을 입력해주세요."},
+    ROOM_CODE_ERROR: {"isSuccess": false, "code": 2004, "message": "유효하지 않은 코드 번호입니다."},
+    ROOM_CAPACITY_EXCESS_ERROR: {"isSuccess": false, "code": 2005, "message": "[정원 초과] 입장할 수 없는 방입니다."},
+    ROOM_STATUS_ERROR: {"isSuccess": false, "code": 2006, "message": "[게임 진행중] 입장할 수 없는 방입니다."},
+    ROOM_CAPACITY_ERROR: {"isSuccess": false, "code": 2007, "message": "방 정원(4~6)을 확인해주세요."},
 
+    // Response error
+    SIGNIN_WITHDRAWAL_ACCOUNT: {"isSuccess": false, "code": 3001, "message": "탈퇴 된 계정입니다. 고객센터에 문의해주세요."},
+    NEW_ROOM_ERROR: {"isSuccess": false, "code": 3002, "message": "방 생성에 실패했습니다."},
+    JOIN_ROOM_ERROR: {"isSuccess": false, "code": 3003, "message": "방 입장에 실패했습니다."},
+    ROOMINFO_MODIFY_ERROR: {"isSuccess": false, "code": 3003, "message": "방 정보 수정에 실패했습니다."},
 
     //Connection, Transaction 등의 서버 오류
-    DB_ERROR : { "isSuccess": false, "code": 4000, "message": "데이터 베이스 에러"},
-    SERVER_ERROR : { "isSuccess": false, "code": 4001, "message": "서버 에러"},
- 
+    DB_ERROR: {"isSuccess": false, "code": 4000, "message": "데이터 베이스 에러"},
+    SERVER_ERROR: {"isSuccess": false, "code": 4001, "message": "서버 에러"},
+
 }
--- src/routes/room/room.js
+++ src/routes/room/room.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-console.log("room 구현중");
+const {Room} = require("../../entities/Room");
+
+const {getRepository} = require("typeorm");
+const {response, errResponse} = require("../../../config/response");
+const baseResponse = require("../../../config/baseResponseStatus");
+
+router.patch('/:roomId/roomInfo', async (req, res)=> {
+    try {
+        let roomId = req.params.roomId;
+        let {roomName, maxPlayer} = req.body;
+
+        let roomRepository = getRepository(Room);
+        let room = await roomRepository.findOne({roomId});
+
+        if(maxPlayer < room.playerCnt || maxPlayer < 4 || maxPlayer > 6) {
+            res.send(errResponse(baseResponse.ROOM_CAPACITY_ERROR));
+        } else {
+            room.maxPlayer = maxPlayer;
+            room.roomName = roomName;
+            await roomRepository.save(room);
+
+            res.send(response(baseResponse.SUCCESS));
+        }
+    } catch (error) {
+        res.send(errResponse(baseResponse.ROOMINFO_MODIFY_ERROR));
+    }
+})
 
 module.exports = router;
