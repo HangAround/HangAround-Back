00f9f3edeae1857c1c4b4a13de4ed7b45d71a758
2214yj <2214yjgm@gmail.com>
2022-03-27 03:13:34 +0900

    FIX: fix local-passport to custom-passport
    ** 로그인 없이 입장 시, 자유롭게 custom 가능한 custom-passport를 사용하도록 수정

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,10 @@ const passport = require('passport');
 const {User} = require("../entities/User");
 const {getRepository} = require("typeorm");
 const baseResponse = require("../../config/baseResponseStatus");
-const {response} = require("../../config/response");
+const {response, errResponse} = require("../../config/response");
+const jwt = require("jsonwebtoken");
+const {httpOnly} = require("express-session/session/cookie");
+const {USER_LOCALPASSPORT_ERROR} = require("../../config/baseResponseStatus");
 
 const router = express.Router();
 
@@ -12,21 +15,39 @@ router.get('/kakao', passport.authenticate('kakao'));
 router.get('/kakao/callback', passport.authenticate('kakao', {
     failureRedirect: '/login',
 }), (req, res) => {
+    const token = jwt.sign({
+        id: req.user.userId,
+    }, process.env.JWT_SECRET, {
+        expiresIn: '12h',
+        issuer: 'hangaround',
+    });
+    res.cookie("loginToken", token, {httpOnly: true});
     res.redirect('/main');
 });
 
 router.get('/without-login', async (req, res, next) => {
-    try {
-        const userRepository = getRepository(User);
-        const newUser = await userRepository.create({
-            channel: "local"
+    passport.authenticate('local', async (authError, user) => {
+        if (authError) {
+            console.error(authError);
+            return errResponse(USER_LOCALPASSPORT_ERROR);
+        }
+
+        return req.login(user, (loginError) => {
+            if (loginError) {
+                console.error(loginError);
+                return errResponse(USER_LOCALPASSPORT_ERROR);
+            }
+
+            const token = jwt.sign({
+                id: req.user.userId,
+            }, process.env.JWT_SECRET, {
+                expiresIn: '12h',
+                issuer: 'hangaround',
+            });
+            res.cookie("loginToken", token, {httpOnly: true});
+            res.redirect('/main');
         });
-        await userRepository.save(newUser);
-        res.send(response(baseResponse.SUCCESS));
-    } catch (error) {
-        console.error(error);
-        res.send(baseResponse.DB_ERROR)
-    }
-})
+    })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
+});
 
 module.exports = router;
