3bd5669437d6399ba269195fafa6da45dcb81af8
Jinjoo Rho <estelle3785@gmail.com>
2022-03-31 22:14:00 +0900

    REFACTOR: Refactor liar game.

--- src/routes/room/game/index.js
+++ src/routes/room/game/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const liarGame = require('./liarGame.js');
 
-router.use('/liar_game', liarGame);
+router.use('/:roomCode/liar_game', liarGame);
 
 module.exports = router;
