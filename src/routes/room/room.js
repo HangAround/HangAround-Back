43f7a3bbc4f296edeecf99439f6f46d1dc79a501
sumin <3189min@naver.com>
2022-03-31 20:21:52 +0900

    REFACTOR: Refactor room.js

--- src/routes/room/room.js
+++ src/routes/room/room.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../../entities/User");
-const { Room } = require("../../entities/Room");
-const { getRepository, getConnection } = require("typeorm");
+const {User} = require("../../entities/User");
+const {Room} = require("../../entities/Room");
+const {getRepository, getConnection} = require("typeorm");
 const baseResponse = require("../../../config/baseResponseStatus");
-const { errResponse, response } = require("../../../config/response");
+const {errResponse, response} = require("../../../config/response");
 const {isLoggedIn, verifyToken} = require("../middleware");
 
 //room 정보 수정
-router.patch('/:roomId/roomInfo', isLoggedIn, verifyToken, async (req, res) => {
+router.patch('/:roomCode/roomInfo', isLoggedIn, verifyToken, async (req, res) => {
     try {
-        let roomId = req.params.roomId;
-        let { roomName, maxPlayer } = req.body;
+        let roomCode = req.params.roomCode;
+        let {roomName, maxPlayer} = req.body;
 
         let roomRepository = getRepository(Room);
-        let room = await roomRepository.findOne({ roomId });
+        let room = await roomRepository.findOne({roomCode});
 
         if (maxPlayer < room.playerCnt || maxPlayer < 4 || maxPlayer > 6) {
             res.send(errResponse(baseResponse.ROOM_CAPACITY_ERROR));
@@ -45,7 +45,7 @@ router.get('/:roomId/roomInfo', isLoggedIn, verifyToken, async (req, res) => {
 router.get('/:roomId/userInfo', isLoggedIn, verifyToken, async (req, res) => {
     const userRepository = getRepository(User);
     const users = await userRepository.find({
-        where: { room: req.params.roomId }
+        where: {room: req.params.roomId}
     })
     if (!users.length) {
         console.log("해당 방에 참여한 유저가 없습니다. 해당 방을 삭제합니다.");
@@ -57,9 +57,9 @@ router.get('/:roomId/userInfo', isLoggedIn, verifyToken, async (req, res) => {
 });
 
 //게임 선택
-router.patch('/:roomId/gameStart/:gameId', isLoggedIn, verifyToken, async(req, res) => {
+router.patch('/:roomCode/gameStart/:gameId', isLoggedIn, verifyToken, async (req, res) => {
     const roomRepository = getRepository(Room);
-    const room = await roomRepository.findOne(req.params.roomId);
+    const room = await roomRepository.findOne(req.params.roomCode);
 
     room.gameId = req.params.gameId;
     await roomRepository.save(room);
@@ -67,20 +67,21 @@ router.patch('/:roomId/gameStart/:gameId', isLoggedIn, verifyToken, async(req, r
     res.send(response(baseResponse.SUCCESS));
 });
 
+
 //room 삭제 혹은 퇴장
 router.delete('/:userId/delete_room', async (req, res) => {
     const userRepository = getRepository(User);
-    const user = await userRepository.findOne(req.params.userId,{
+    const user = await userRepository.findOne(req.params.userId, {
         relations: ['room'],
     });
     const roomRepository = getRepository(Room);
-    const room = await roomRepository.findOne(user.room);    
+    const room = await roomRepository.findOne(user.room);
 
     if (room.gameId === 1) {
         if (room.ownerId === req.params.userId) {
             const users = await getRepository(User)
                 .createQueryBuilder()
-                .where({ room: room.roomId })
+                .where({room: room.roomId})
                 .getMany();
             for (i = 0; i < users.length; i++) {
                 await getConnection()
@@ -98,11 +99,10 @@ router.delete('/:userId/delete_room', async (req, res) => {
                 .createQueryBuilder()
                 .delete()
                 .from(Room)
-                .where({ ownerId: req.params.userId })
+                .where({ownerId: req.params.userId})
                 .execute();
             res.send(response(baseResponse.DELETE_ROOM_SUCCESS));
-        }
-        else {
+        } else {
             await getConnection()
                 .createQueryBuilder()
                 .update(User)
@@ -115,8 +115,7 @@ router.delete('/:userId/delete_room', async (req, res) => {
                 .execute();
             res.send(response(baseResponse.EXIT_ROOM_SUCCESS));
         }
-    }
-    else {
+    } else {
         res.send(response(baseResponse.EXIT_ROOM_ERROR));
     }
 });
