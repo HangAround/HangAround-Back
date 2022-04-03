1935a6f3038980dd615301880ee5ad4baa82cce6
kim-yeji <2214yjgm@gmail.com>
2022-04-03 12:59:11 +0900

    FEAT: Add consonant in consonantGame #43

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const liarGame = require('./liarGame.js');
+const consonantGame = require('./consonantGame.js');
 
 router.use('/:roomCode/liar_game', liarGame);
+router.use('/:roomCode/consonant_game',consonantGame);
 
 module.exports = router;
