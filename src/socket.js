15facd880b6be68d5d39227d1ca3f09dd772ad9d
kim-yeji <2214yjgm@gmail.com>
2022-05-12 20:12:01 +0900

    FEAT: gameStart Socket Event

--- src/socket.js
+++ src/socket.js
@@ -1,22 +1,49 @@
 const SocketIO = require('socket.io');
-
+const consonantGame = require('./routes/game/consonantGame');
 
 module.exports = (server, app) => {
     const io = SocketIO(server);
-
-    io.on('connection', (socket) => {
-    console.log('소켓 접속');
-
-    socket.on('ping',msg => {
-        console.log(msg);
-    })
-    });
-
     app.set('io', io);
-    const gameRoom = io.of('/consonantGame');
 
-    gameRoom.on('connection', (socket) => {
-        console.log('room 네임스페이스에 접속');
+    //네임스페이스 및 룸 세팅
+    const consonantGame = io.of('/consonantGame');
+    let rooms = [];
+
+    consonantGame.on('connection', (socket) => {
+        console.log('gameRoom 네임스페이스에 접속');
+        let roomCode, name;
+
+        //게임 시작, 방에 join하고 랜덤 초성 알림
+        socket.on('gameStart', (data) => {
+            const obj = JSON.parse(data);
+            roomCode = obj.roomCode;
+            name = obj.name;
+
+            if(!rooms.includes(roomCode)) {
+                rooms.push(roomCode);
+            }
+            console.log(rooms);
+            socket.join(roomCode, () => {
+                console.log('join');
+                console.log(name + ' join a ' + roomCode);
+                let consonant = consonantGame.randomConsonant();
+                consonantGame.to(roomCode).emit('consonant', {'consonant': consonant});
+
+            });
+            console.log('test end');
+        });
+
+        //타이머 세팅
+        socket.on('response', () => {
+            const timeout = setTimeout(() => {
+                console.log('timeOver');
+                consonantGame.to(roomCode).emit('timeOver');
+            }, 180000);
+            console.log("3분 타이머 세팅")
+            consonantGame.to(roomCode).emit('timerStart');
+        })
+
+
         socket.on('disconnect', () => {
             console.log('room 네임스페이스 접속 해제');
         });
