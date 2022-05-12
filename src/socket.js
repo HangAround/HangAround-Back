f495312f6fd7f8d27afa9bf08c36fc65ddbf87c9
sumin <3189min@naver.com>
2022-05-13 00:02:39 +0900

    FEAT: Add gameOver socket

--- src/socket.js
+++ src/socket.js
@@ -1,25 +1,52 @@
 const SocketIO = require('socket.io');
 
+const {Room} = require("./entities/Room");
+const {getRepository} = require("typeorm");
 
 module.exports = (server, app) => {
     const io = SocketIO(server);
+    app.set('io', io);
 
     io.on('connection', (socket) => {
-    console.log('소켓 접속');
+        console.log('소켓 접속');
 
-    socket.on('ping',msg => {
-        console.log(msg);
-    })
+        socket.on('ping',msg => {
+            console.log(msg);
+        })
     });
 
     app.set('io', io);
     const gameRoom = io.of('/consonantGame');
 
     gameRoom.on('connection', (socket) => {
-        console.log('room 네임스페이스에 접속');
+        console.log('game 네임스페이스에 접속');
+
+        let count = 0;
+        let roomCode = "";
+        let players = 0;
+        socket.on('userId', async (data) => {
+            roomCode = data.roomCode;
+            socket.join(roomCode); //룸 접속
+
+            let roomRepository = getRepository(Room);
+            let room = await roomRepository.findOne({roomCode});
+            if (room !== undefined) {
+                players = room.playerCnt;
+                console.log(players);
+                if(count === players-1) {
+                    app.get('io').of('/consonantGame').to(roomCode).emit('gameOver', `게임이 종료되었습니다.`);
+                } else {
+                    count++;
+                    app.get('io').of('/consonantGame').to(roomCode).emit('notice', `${data.userId}님 정답입니다!`);
+                }
+            } else {
+                console.log('room from database is not defined!');
+            }
+        });
+
         socket.on('disconnect', () => {
-            console.log('room 네임스페이스 접속 해제');
+            console.log('game 네임스페이스 접속 해제');
+            socket.leave(roomCode);
         });
     });
-
 };
