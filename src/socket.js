94e2bdb6769e799b7ef5d828f021017b13313a78
kim-yeji <2214yjgm@gmail.com>
2022-05-13 00:09:19 +0900

    REFACTOR: join room in socket v2
    ** room 객체 배열을 통해 room 정보를 관리하지 않는 버전
    ** 아무도 room 객체 배열을 사용하지 않을 경우 채택

--- src/socket.js
+++ src/socket.js
@@ -7,37 +7,18 @@ module.exports = (server, app) => {
 
     //네임스페이스 및 룸 세팅
     const gameRoom = io.of('/gameRoom');
-    let rooms = [];
     let roomCode, name;
 
     //socket connection
     gameRoom.on('connection', (socket) => {
         console.log('gameRoom 네임스페이스에 접속');
 
-        //room 입장
-        socket.on('join', (data) => {
+        //초성 알림
+        socket.on('gameStart', async (data) => {
             const obj = JSON.parse(data);
             roomCode = obj.roomCode;
             name = obj.name;
 
-            if (rooms.findIndex(room => room.roomCode === roomCode) === -1) {
-                //만약 roomCode 일치하는 room 없으면 새로운 room 객체(roomCode, 현재인원) 생성 후 room 객체 배열에 집어넣음
-                let roomInfo = {'roomCode': roomCode, 'cnt': 1}
-                rooms.push(roomInfo);
-            } else {
-                //만약 일치하는 room 있으면 현재인원만 +1
-                let index = rooms.findIndex(room => room.roomCode === roomCode);
-                rooms[index].cnt++;
-            }
-            socket.join(roomCode);
-            socket.to(roomCode).emit('join', {
-                msg: `${name}님이 입장했습니다.`
-            });
-            console.log(name + ' join a ' + roomCode);
-        });
-
-        socket.on('gameStart', async () => {
-            //초성 알림
             let consonant = randomConsonant.randomConsonant();
             socket.to(roomCode).emit('consonant', {'consonant': consonant});
             console.log('consonant is ' + consonant);
